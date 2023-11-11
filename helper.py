ab4c340629386b8dcb290beb2e9ba421479f09c1
Olympiah <87874970+Olympiah@users.noreply.github.com>
2023-11-11 08:26:45 +0300

    Added helper functions🐬Also tested the OCR with the license plates but fixing an error with the tracker

--- helper.py
+++ helper.py
@@ -4,6 +4,21 @@ import easyocr
 # Initialize the OCR reader
 reader = easyocr.Reader(['en'], gpu=False)
 
+# Mapping dictionaries for character conversion
+dict_char_to_int = {'O': '0',
+                    'I': '1',
+                    'J': '3',
+                    'A': '4',
+                    'G': '6',
+                    'S': '5'}
+
+dict_int_to_char = {'0': 'O',
+                    '1': 'I',
+                    '3': 'J',
+                    '4': 'A',
+                    '6': 'G',
+                    '5': 'S'}
+
 
 def write_csv(results, output_path):
     """
@@ -27,29 +42,18 @@ def write_csv(results, output_path):
                             f.write('{},{},{},{},{},{},{}\n'.format(frame_nmr,
                                                                     vehicle_id,
                                                                     '[{} {} {} {}]'.format(
-                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][
-                                                                            0],
-                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][
-                                                                            1],
-                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][
-                                                                            2],
-                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][
-                                                                            3]),
+                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][0],
+                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][1],
+                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][2],
+                                                                        results[frame_nmr][vehicle_id]['car']['bbox'][3]),
                                                                     '[{} {} {} {}]'.format(
-                                                                        results[frame_nmr][vehicle_id]['license_plate'][
-                                                                            'bbox'][0],
-                                                                        results[frame_nmr][vehicle_id]['license_plate'][
-                                                                            'bbox'][1],
-                                                                        results[frame_nmr][vehicle_id]['license_plate'][
-                                                                            'bbox'][2],
-                                                                        results[frame_nmr][vehicle_id]['license_plate'][
-                                                                            'bbox'][3]),
-                                                                    results[frame_nmr][vehicle_id]['license_plate'][
-                                                                        'bbox_score'],
-                                                                    results[frame_nmr][vehicle_id]['license_plate'][
-                                                                        'text'],
-                                                                    results[frame_nmr][vehicle_id]['license_plate'][
-                                                                        'text_score'])
+                                                                        results[frame_nmr][vehicle_id]['license_plate']['bbox'][0],
+                                                                        results[frame_nmr][vehicle_id]['license_plate']['bbox'][1],
+                                                                        results[frame_nmr][vehicle_id]['license_plate']['bbox'][2],
+                                                                        results[frame_nmr][vehicle_id]['license_plate']['bbox'][3]),
+                                                                    results[frame_nmr][vehicle_id]['license_plate']['bbox_score'],
+                                                                    results[frame_nmr][vehicle_id]['license_plate']['text'],
+                                                                    results[frame_nmr][vehicle_id]['license_plate']['text_score'])
                                     )
         f.close()
 
@@ -68,12 +72,12 @@ def license_complies_format(text):
         return False
 
     if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
-            (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
-            (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
-            (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
-            (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
-            (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
-            (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
         return True
     else:
         return False
